ec597dce6a09f1d6507586cf2cf99494b99fa81b
J-P Nurmi <jp.nurmi@canonical.com>
2023-08-07 18:21:17 +0200

    feat(init): add libsysmetrics FFI bindings (#65)

---
+++ packages/sysmetrics/lib/src/libsysmetrics.dart
@@ -0,0 +1,12 @@
+import 'dart:ffi' as ffi;
+
+import 'package:meta/meta.dart';
+
+import 'libsysmetrics.g.dart';
+
+LibSysmetrics? _lib;
+LibSysmetrics get lib =>
+    _lib ??= LibSysmetrics(ffi.DynamicLibrary.open('libsysmetrics.so.1'));
+
+@visibleForTesting
+void overrideLibSysmetricsForTesting(LibSysmetrics lib) => _lib = lib;
