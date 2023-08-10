640d248cefdf3c75540848e2b2cdc509c170cf76
J-P Nurmi <jp.nurmi@canonical.com>
2023-08-10 18:24:42 +0200

    fix(store): remove can-launch check

--- packages/ubuntu_init/test/store/store_model_test.dart
+++ packages/ubuntu_init/test/store/store_model_test.dart
@@ -7,16 +7,10 @@ import 'test_store.dart';
 void main() {
   test('launcher', () async {
     final launcher = MockUrlLauncher();
-    when(launcher.canLaunchUrl(kStoreUrl)).thenAnswer((_) async => true);
     when(launcher.launchUrl(kStoreUrl)).thenAnswer((_) async => true);
 
     final model = StoreModel(launcher);
     expect(await model.init(), isTrue);
-    verify(launcher.canLaunchUrl(kStoreUrl)).called(1);
-
-    when(launcher.canLaunchUrl(kStoreUrl)).thenAnswer((_) async => false);
-    expect(await model.init(), isFalse);
-    verify(launcher.canLaunchUrl(kStoreUrl)).called(1);
 
     await model.launch();
     verify(launcher.launchUrl(kStoreUrl)).called(1);
