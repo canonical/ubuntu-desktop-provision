5002586aa6fc5c5e03e49313b677fb718dec599a
Sergio Costas Rodriguez <sergio.costas@canonical.com>
2023-09-29 17:23:14 +0200

    Always ask for a new connection

--- packages/ubuntu_provision/lib/src/services/gdm_service.dart
+++ packages/ubuntu_provision/lib/src/services/gdm_service.dart
@@ -9,7 +9,6 @@ import 'package:ubuntu_service/ubuntu_service.dart';
 final log = Logger('dart_gdm_service');
 
 class GdmService {
-  DBusClient? _connection;
   bool _testing = false;
 
   void setTesting() {
@@ -17,17 +16,14 @@ class GdmService {
   }
 
   Future<DBusClient> getClientConnection() async {
-    if (_connection == null) {
-      var gdmObject = DBusRemoteObject(DBusClient.system(),
-          name: 'org.gnome.DisplayManager',
-          path: DBusObjectPath('/org/gnome/DisplayManager/Manager'));
-      var response = await gdmObject.callMethod(
-          'org.gnome.DisplayManager.Manager', 'OpenSession', [],
-          replySignature: DBusSignature('s'));
-      var address = response.values[0].asString();
-      _connection = DBusClient(DBusAddress(address), messageBus: false);
-    }
-    return _connection!;
+    var gdmObject = DBusRemoteObject(DBusClient.system(),
+        name: 'org.gnome.DisplayManager',
+        path: DBusObjectPath('/org/gnome/DisplayManager/Manager'));
+    var response = await gdmObject.callMethod(
+        'org.gnome.DisplayManager.Manager', 'OpenSession', [],
+        replySignature: DBusSignature('s'));
+    var address = response.values[0].asString();
+    return DBusClient(DBusAddress(address), messageBus: false);
   }
 
   Future<DBusRemoteObject> getGreeter() async {
