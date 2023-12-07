f539c4f636f857cd3c8410554923be3543998ede
Dennis Loose <dennis.loose@canonical.com>
2023-12-07 15:02:08 +0100

    feat(privacy_service): catch and log errors

--- packages/ubuntu_init/lib/src/services/privacy_service.dart
+++ packages/ubuntu_init/lib/src/services/privacy_service.dart
@@ -1,7 +1,10 @@
 import 'package:dbus/dbus.dart';
 import 'package:gsettings/gsettings.dart';
 import 'package:meta/meta.dart';
+import 'package:ubuntu_logger/ubuntu_logger.dart';
 import 'package:ubuntu_service/ubuntu_service.dart';
+
+final _log = Logger('gnome_privacy_service');
 
 abstract class PrivacyService {
   Future<bool> isLocationEnabled();
@@ -26,26 +29,47 @@ class GnomePrivacyService implements PrivacyService {
   final GSettings _privacySettings;
 
   @override
-  Future<bool> isLocationEnabled() {
-    return _locationSettings.get('enabled').then((v) => v.asBoolean());
+  Future<bool> isLocationEnabled() async {
+    try {
+      return await _locationSettings.get('enabled').then((v) => v.asBoolean());
+    } catch (e) {
+      _log.error('Error getting location settings: $e');
+      return false;
+    }
   }
 
   @override
-  Future<void> setLocationEnabled(bool enabled) {
-    return _locationSettings.set('enabled', DBusBoolean(enabled));
+  Future<void> setLocationEnabled(bool enabled) async {
+    setReportingEnabled(true);
+    try {
+      return await _locationSettings.set('enabled', DBusBoolean(enabled));
+    } catch (e) {
+      _log.error('Error setting location settings: $e');
+      return;
+    }
   }
 
   @override
-  Future<bool> isReportingEnabled() {
-    return _privacySettings
-        .get('report-technical-problems')
-        .then((v) => v.asBoolean());
+  Future<bool> isReportingEnabled() async {
+    try {
+      return await _privacySettings
+          .get('report-technical-problems')
+          .then((v) => v.asBoolean());
+    } catch (e) {
+      _log.error('Error getting privacy settings: $e');
+      return false;
+    }
   }
 
   @override
-  Future<void> setReportingEnabled(bool enabled) {
-    return _privacySettings.set(
-        'report-technical-problems', DBusBoolean(enabled));
+  Future<void> setReportingEnabled(bool enabled) async {
+    try {
+      return await _privacySettings.set(
+          'report-technical-problems', DBusBoolean(enabled));
+    } catch (e) {
+      _log.error('Error setting privacy settings: $e');
+      return;
+    }
   }
 
   @override
