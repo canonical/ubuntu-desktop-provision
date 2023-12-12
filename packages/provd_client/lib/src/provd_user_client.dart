138270077e499fbeb81515403f22a27bb3d5b4a1
Lukas Klingsbo <lukas.klingsbo@gmail.com>
2023-12-12 11:14:32 +0100

    chore: Exclude pb files in analysis

--- packages/provd_client/lib/src/provd_user_client.dart
+++ packages/provd_client/lib/src/provd_user_client.dart
@@ -4,9 +4,8 @@ import 'package:grpc/grpc.dart';
 
 import 'generated/user.pbgrpc.dart';
 
+/// Handles the communication with provd regarding user actions.
 class ProvdUserClient {
-  final UserServiceClient _userClient;
-
   ProvdUserClient(String serverUrl, int port)
       : _userClient = UserServiceClient(
           ClientChannel(
@@ -18,17 +17,22 @@ class ProvdUserClient {
           ),
         );
 
+  final UserServiceClient _userClient;
+
+  /// Gets the user with [userId].
   Future<User> getUser(String userId) async {
     final request = GetUserRequest(userId: userId);
     final grpcResponse = await _userClient.getUser(request);
     return grpcResponse.user;
   }
 
+  /// Creates a new user from [user].
   Future<void> createUser(User user) async {
     final request = CreateUserRequest(identity: user);
     await _userClient.createUser(request);
   }
 
+  /// Validates the [username] and returns true if it is valid.
   Future<bool> validateUsername(String username) async {
     final request = ValidateUsernameRequest(username: username);
     final response = await _userClient.validateUsername(request);
