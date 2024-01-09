e21efcc517327d9818d7729a4a209a1bc943f48b
matt-hagemann <matt.hagemann@canonical.com>
2024-01-09 16:55:42 +0200

    test: CreateUser tests

--- provd/internal/services/user/user_test.go
+++ provd/internal/services/user/user_test.go
@@ -2,8 +2,6 @@ package user_test
 
 import (
 	"context"
-	"flag"
-	"fmt"
 	"net"
 	"os"
 	"path/filepath"
@@ -12,6 +10,7 @@ import (
 	"github.com/canonical/ubuntu-desktop-provision/provd/internal/services/user"
 	"github.com/canonical/ubuntu-desktop-provision/provd/internal/testutils"
 	"github.com/canonical/ubuntu-desktop-provision/provd/proto"
+	"github.com/godbus/dbus/v5"
 	"github.com/stretchr/testify/require"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials/insecure"
@@ -27,20 +26,90 @@ func TestCreateUser(t *testing.T) {
 		hostname  string
 		autoLogin bool
 
+		accountsError bool
+		hostnameError bool
+
 		wantErr bool
 	}{
-		"Successfully creates a user":  {realName: "Matt", username: "matt", password: "password", hostname: "ubuntu", autoLogin: true},
-		"Error when realName is empty": {realName: "", username: "matt", password: "password", hostname: "ubuntu", autoLogin: true, wantErr: true},
-		"Error when username is empty": {realName: "Matt", username: "", password: "password", hostname: "ubuntu", autoLogin: true, wantErr: true},
-		"Error when hostname is empty": {realName: "Matt", username: "matt", password: "password", hostname: "", autoLogin: true, wantErr: true},
+		"Successfully creates a user": {
+			realName:  "Matt",
+			username:  "matt",
+			password:  "password",
+			hostname:  "ubuntu",
+			autoLogin: true,
+		},
+		"Error when realName is empty": {
+			realName:  "",
+			username:  "matt",
+			password:  "password",
+			hostname:  "ubuntu",
+			autoLogin: true,
+			wantErr:   true,
+		},
+		"Error when username is empty": {
+			realName:  "Matt",
+			username:  "",
+			password:  "password",
+			hostname:  "ubuntu",
+			autoLogin: true,
+			wantErr:   true,
+		},
+		"Error when hostname is empty": {
+			realName:  "Matt",
+			username:  "matt",
+			password:  "password",
+			hostname:  "",
+			autoLogin: true,
+			wantErr:   true,
+		},
+		"Error from Accounts service": {
+			realName:      "Matt",
+			username:      "matt",
+			password:      "password",
+			hostname:      "ubuntu",
+			autoLogin:     true,
+			accountsError: true,
+			wantErr:       true,
+		},
+		"Error from Hostname service": {
+			realName:      "Matt",
+			username:      "matt",
+			password:      "password",
+			hostname:      "ubuntu",
+			autoLogin:     true,
+			hostnameError: true,
+			wantErr:       true,
+		},
 	}
 
 	for name, tc := range tests {
 		tc := tc
 		t.Run(name, func(t *testing.T) {
 			t.Parallel()
+			t.Cleanup(testutils.StartLocalSystemBus())
 
-			client := newUserClient(t)
+			accountsMock := &user.AccountsObjectMock{
+				UserObjectPath: dbus.ObjectPath("/org/freedesktop/Accounts/User1000"),
+				WantError:      tc.accountsError,
+			}
+
+			hostnameMock := &user.HostnameObjectMock{
+				Hostname:  tc.hostname,
+				WantError: tc.hostnameError,
+			}
+
+			userFactoryMock := user.UserObjectFactoryMock{
+				&user.UserObjectMock{
+					Properties: map[string]interface{}{
+						"RealName":  tc.realName,
+						"UserName":  tc.username,
+						"Password":  tc.password,
+						"AutoLogin": tc.autoLogin,
+					},
+				},
+			}
+
+			client := newUserClient(t, accountsMock, hostnameMock, userFactoryMock)
 
 			userReq := &proto.CreateUserRequest{
 				User: &proto.User{
@@ -52,23 +121,18 @@ func TestCreateUser(t *testing.T) {
 				},
 			}
 
-			userResp, err := client.CreateUser(context.Background(), userReq)
+			_, err := client.CreateUser(context.Background(), userReq)
 			if tc.wantErr {
 				require.Error(t, err, "CreateUser should return an error, but did not")
 				return
 			}
 			require.NoError(t, err, "CreateUser should not return an error, but did")
-
-			got := userResp
-			want := testutils.LoadWithUpdateFromGolden(t, got.String())
-			require.Equal(t, want, got, "CreateUser returned an unexpected response")
 		})
 	}
 }
 
-func newUserClient(t *testing.T) (client proto.UserServiceClient) {
+func newUserClient(t *testing.T, accountsMock user.Caller, hostnameMock user.Caller, userFactoryMock user.UserObjectFactory) proto.UserServiceClient {
 	t.Helper()
-
 	// socket path is limited in length.
 	tmpDir, err := os.MkdirTemp("", "hello-socket-dir")
 	require.NoError(t, err, "Setup: could not setup temporary socket dir path")
@@ -78,10 +142,13 @@ func newUserClient(t *testing.T) (client proto.UserServiceClient) {
 	lis, err := net.Listen("unix", socketPath)
 	require.NoError(t, err, "Setup: could not create unix socket")
 
-	service := user.Service{}
+	bus := testutils.NewDbusConn(t)
+
+	// Create the service with the necessary mocks
+	service := user.New(bus, user.WithAccounts(accountsMock), user.WithHostname(hostnameMock), user.WithUserFactory(userFactoryMock))
 
 	grpcServer := grpc.NewServer()
-	proto.RegisterUserServiceServer(grpcServer, &service)
+	proto.RegisterUserServiceServer(grpcServer, service)
 	done := make(chan struct{})
 	go func() {
 		defer close(done)
@@ -97,20 +164,4 @@ func newUserClient(t *testing.T) (client proto.UserServiceClient) {
 	t.Cleanup(func() { _ = conn.Close() })
 
 	return proto.NewUserServiceClient(conn)
-}
-
-func TestMain(m *testing.M) {
-	testutils.InstallUpdateFlag()
-	flag.Parse()
-
-	// Start system bus mock.
-	busCleanup, err := testutils.StartSystemBusMock()
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "Failed to start system bus mock: %v\n", err)
-		os.Exit(1)
-	}
-	defer busCleanup()
-
-	// Run the tests.
-	os.Exit(m.Run())
 }
