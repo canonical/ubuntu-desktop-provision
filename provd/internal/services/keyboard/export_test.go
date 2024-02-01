cd7dde4763c000d602929188ec59ae6722aff331
matt-hagemann <matt.hagemann@canonical.com>
2024-02-01 08:29:28 +0200

    test(provd): SetKeyboard tests

---
+++ provd/internal/services/keyboard/export_test.go
@@ -0,0 +1,23 @@
+package keyboard
+
+import (
+	"errors"
+
+	"github.com/canonical/ubuntu-desktop-provision/provd/internal/consts"
+	"github.com/godbus/dbus/v5"
+)
+
+// WithLocalePath is a functional option to set the DBus locale path.
+func WithLocalePath(path string) Option {
+	return func(s *Service) error {
+		s.locale = s.conn.Object(consts.DbusLocalePrefix, dbus.ObjectPath("/org/freedesktop/"+path))
+		if s.locale == nil {
+			return errors.New("invalid locale path")
+		}
+		err := s.locale.Call(consts.DbusPeerPrefix+".Ping", 0).Err
+		if err != nil {
+			return errors.New("invalid locale path")
+		}
+		return nil
+	}
+}
---
+++ provd/internal/services/keyboard/keyboard_test.go
@@ -0,0 +1,166 @@
+package keyboard_test
+
+import (
+	"context"
+	"flag"
+	"fmt"
+	"log/slog"
+	"net"
+	"os"
+	"path/filepath"
+	"testing"
+
+	"github.com/canonical/ubuntu-desktop-provision/provd/internal/services/keyboard"
+	"github.com/canonical/ubuntu-desktop-provision/provd/internal/testutils"
+	pb "github.com/canonical/ubuntu-desktop-provision/provd/protos"
+	"github.com/stretchr/testify/require"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+)
+
+func TestEmptySetInputSourcesRequest(t *testing.T) {
+	t.Parallel()
+
+	client := newKeyboardClient(t)
+
+	keyboardResp, err := client.SetInputSource(context.Background(), nil)
+	require.Error(t, err, "SetInputSources should return an error for nil request")
+	require.Empty(t, keyboardResp, "SetInputSources should return a nil response for a nil request")
+}
+
+func TestEmptySetKeyboardRequest(t *testing.T) {
+	t.Parallel()
+
+	client := newKeyboardClient(t)
+
+	keyboardResp, err := client.SetKeyboard(context.Background(), nil)
+	require.Error(t, err, "SetKeyboard should return an error for nil request")
+	require.Empty(t, keyboardResp, "SetKeyboard should return a nil response for a nil request")
+}
+
+func TestSetKeyboard(t *testing.T) {
+	t.Parallel()
+
+	tests := map[string]struct {
+		variant     string
+		layout      string
+		locale1Path string
+		wantErr     bool
+	}{
+		// Success cases
+		"Success on valid layout and variant":    {},
+		"Success on valid layout, empty variant": {variant: "-"},
+
+		// Error cases
+		"Error on empty layout": {layout: "-", wantErr: true},
+
+		// Dbus errors
+		"Error when gettings X11Model":   {locale1Path: "x11modelerror", wantErr: true},
+		"Error when getting X11Options":  {locale1Path: "x11optionserror", wantErr: true},
+		"Error from locale dbus object":  {locale1Path: "localeerror", wantErr: true},
+		"Error when setting X11Keyboard": {layout: "x11keyboarderror", wantErr: true},
+	}
+
+	for name, tc := range tests {
+		tc := tc
+		t.Run(name, func(t *testing.T) {
+			t.Parallel()
+			t.Cleanup(testutils.StartLocalSystemBus())
+
+			var opts []keyboard.Option
+			if tc.locale1Path != "" {
+				opts = append(opts, keyboard.WithLocalePath(tc.locale1Path))
+			}
+
+			client := newKeyboardClient(t, opts...)
+
+			if tc.variant == "" {
+				tc.variant = "ok"
+			} else if tc.variant == "-" {
+				tc.variant = ""
+			}
+
+			if tc.layout == "" {
+				tc.layout = "ok"
+			} else if tc.layout == "-" {
+				tc.layout = ""
+			}
+
+			keyboardReq := &pb.SetKeyboardRequest{
+				Settings: &pb.KeyboardSettings{
+					Layout:  tc.layout,
+					Variant: tc.variant,
+				},
+			}
+
+			keyboardResp, err := client.SetKeyboard(context.Background(), keyboardReq)
+			if tc.wantErr {
+				require.Error(t, err, "SetKeyboard should return an error")
+				require.Empty(t, keyboardResp, "SetKeyboard should return a nil response")
+				return
+			}
+			require.NoError(t, err, "SetKeyboard should not return an error")
+		})
+	}
+}
+
+// newKeyboardClient creates a new keyboard client for testing, with a temp unix socket and mock Dbus connection.
+func newKeyboardClient(t *testing.T, opts ...keyboard.Option) pb.KeyboardServiceClient {
+	t.Helper()
+	// socket path is limited in length.
+	tmpDir, err := os.MkdirTemp("", "hello-socket-dir")
+	require.NoError(t, err, "Setup: could not setup temporary socket dir path")
+	t.Cleanup(func() { _ = os.RemoveAll(tmpDir) })
+	socketPath := filepath.Join(tmpDir, "provd.sock")
+
+	lis, err := net.Listen("unix", socketPath)
+	require.NoError(t, err, "Setup: could not create unix socket")
+
+	bus := testutils.NewDbusConn(t)
+
+	service, err := keyboard.New(bus, opts...)
+
+	if err != nil {
+		t.Fatalf("Setup: could not create user service: %v", err)
+	}
+
+	grpcServer := grpc.NewServer()
+	pb.RegisterKeyboardServiceServer(grpcServer, service)
+	done := make(chan struct{})
+	go func() {
+		defer close(done)
+		_ = grpcServer.Serve(lis)
+	}()
+	t.Cleanup(func() {
+		grpcServer.Stop()
+		<-done
+	})
+
+	conn, err := grpc.Dial("unix://"+socketPath, grpc.WithTransportCredentials(insecure.NewCredentials()))
+	require.NoError(t, err, "Setup: Could not connect to GRPC server")
+	t.Cleanup(func() { _ = conn.Close() })
+
+	return pb.NewKeyboardServiceClient(conn)
+}
+
+func TestMain(m *testing.M) {
+	testutils.InstallUpdateFlag()
+	flag.Parse()
+
+	defer testutils.StartLocalSystemBus()()
+
+	conn, err := testutils.GetSystemBusConnection()
+
+	if err != nil {
+		slog.Error(fmt.Sprintf("Could not get system bus connection: %v", err))
+		os.Exit(1)
+	}
+
+	err = testutils.ExportLocaleMock(conn)
+	if err != nil {
+		slog.Error(fmt.Sprintf("Could not export locale1 mock: %v", err))
+		os.Exit(1)
+	}
+
+	m.Run()
+}
