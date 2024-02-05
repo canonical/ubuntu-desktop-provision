f46843e2e17ceb0409582cd6e47f176743333722
Lukas Klingsbo <lukas.klingsbo@gmail.com>
2024-02-05 17:54:52 +0100

    fix: Use `HorizontalLayout` on `RstPage`

--- packages/ubuntu_bootstrap/lib/pages/rst/rst_page.dart
+++ packages/ubuntu_bootstrap/lib/pages/rst/rst_page.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_html/flutter_html.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:flutter_svg/svg.dart';
 import 'package:ubuntu_bootstrap/l10n.dart';
 import 'package:ubuntu_bootstrap/pages/rst/rst_model.dart';
 import 'package:ubuntu_bootstrap/widgets.dart';
@@ -28,75 +27,46 @@ class RstPage extends ConsumerWidget with ProvisioningPage {
     final flavor = ref.watch(flavorProvider);
     final model = ref.watch(rstModelProvider);
     final lang = UbuntuBootstrapLocalizations.of(context);
-    return Scaffold(
-      body: WizardPage(
-        title: YaruWindowTitleBar(
-          title: Text(lang.rstTitle),
-        ),
-        content: Center(
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: <Widget>[
-              Flexible(
-                child: SvgPicture.asset(
-                  'assets/storage/rst.svg',
-                  colorFilter: ColorFilter.mode(
-                    context.isDarkMode ? Colors.white : Colors.black,
-                    BlendMode.srcIn,
-                  ),
-                ),
-              ),
-              const SizedBox(width: 48),
-              Flexible(
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  mainAxisSize: MainAxisSize.min,
-                  children: [
-                    Text(
-                      lang.rstHeader,
-                      style: Theme.of(context).textTheme.headlineSmall,
-                    ),
-                    const SizedBox(height: kWizardSpacing),
-                    Text(lang.rstDescription),
-                    const SizedBox(height: kWizardSpacing),
-                    Html(
-                      data: lang.rstInstructions('help.ubuntu.com/rst'),
-                      style: {
-                        'body': Style(margin: Margins.zero),
-                        'a': Style(color: Theme.of(context).colorScheme.link),
-                      },
-                      onLinkTap: (url, _, __) => launchUrl(url!),
-                    ),
-                    const SizedBox(height: kWizardSpacing),
-                    FilledButton(
-                      onPressed: () async {
-                        final window = YaruWindow.of(context);
-                        final confirmed = await showConfirmationDialog(
-                          context,
-                          title: lang.restartIntoWindowsTitle,
-                          message:
-                              lang.restartIntoWindowsDescription(flavor.name),
-                          okLabel: UbuntuLocalizations.of(context).restartLabel,
-                          okElevated: true,
-                        );
-                        if (confirmed) {
-                          await model.reboot().then((_) => window.close());
-                        }
-                      },
-                      child: Text(
-                        lang.restartIntoWindows,
-                      ),
-                    )
-                  ],
-                ),
-              ),
-            ],
+    return HorizontalPage(
+      windowTitle: lang.rstTitle,
+      title: lang.rstHeader,
+      content: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Text(lang.rstDescription),
+          const SizedBox(height: kWizardSpacing),
+          Html(
+            data: lang.rstInstructions('help.ubuntu.com/rst'),
+            style: {
+              'body': Style(margin: Margins.zero),
+              'a': Style(color: Theme.of(context).colorScheme.link),
+            },
+            onLinkTap: (url, _, __) => launchUrl(url!),
           ),
-        ),
-        bottomBar: const WizardBar(
-          leading: PreviousWizardButton(),
-        ),
+          const SizedBox(height: kWizardSpacing),
+          FilledButton(
+            onPressed: () async {
+              final window = YaruWindow.of(context);
+              final confirmed = await showConfirmationDialog(
+                context,
+                title: lang.restartIntoWindowsTitle,
+                message: lang.restartIntoWindowsDescription(flavor.name),
+                okLabel: UbuntuLocalizations.of(context).restartLabel,
+                okElevated: true,
+              );
+              if (confirmed) {
+                await model.reboot().then((_) => window.close());
+              }
+            },
+            child: Text(
+              lang.restartIntoWindows,
+            ),
+          )
+        ],
+      ),
+      bottomBar: const WizardBar(
+        leading: PreviousWizardButton(),
       ),
     );
   }
