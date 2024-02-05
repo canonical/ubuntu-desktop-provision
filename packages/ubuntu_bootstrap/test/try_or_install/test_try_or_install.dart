ef8a7d8e08f575f0d206562d22e20ada12d091ce
Lukas Klingsbo <lukas.klingsbo@gmail.com>
2024-02-05 17:06:11 +0100

    Remove release notes const from test

--- packages/ubuntu_bootstrap/test/try_or_install/test_try_or_install.dart
+++ packages/ubuntu_bootstrap/test/try_or_install/test_try_or_install.dart
@@ -12,15 +12,12 @@ import 'test_try_or_install.mocks.dart';
 export '../test_utils.dart';
 export 'test_try_or_install.mocks.dart';
 
-const kTestReleastNoteUrl = 'https://wiki.ubuntu.com/foo/ReleaseNotes';
-
 @GenerateMocks([TryOrInstallModel])
 TryOrInstallModel buildTryOrInstallModel(
     {bool? isConnected, TryOrInstallOption? option}) {
   final model = MockTryOrInstallModel();
   when(model.isConnected).thenReturn(isConnected ?? false);
   when(model.option).thenReturn(option ?? TryOrInstallOption.none);
-  when(model.releaseNotesURL(any)).thenReturn(kTestReleastNoteUrl);
   return model;
 }
 
