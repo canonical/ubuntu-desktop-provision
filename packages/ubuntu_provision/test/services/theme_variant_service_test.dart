c1499b589b74a2b7888f334a9e71da5b36e7dcf9
Dennis Loose <dennis.loose@canonical.com>
2024-02-09 12:34:29 +0100

    feat(provision): add dark theme support to ThemeVariantService

--- packages/ubuntu_provision/test/services/theme_variant_service_test.dart
+++ packages/ubuntu_provision/test/services/theme_variant_service_test.dart
@@ -6,11 +6,13 @@ import 'package:ubuntu_provision/theme_variant.dart';
 import '../test_utils.dart';
 
 void main() {
-  test('getThemeVariant', () async {
+  test('getThemeVariant without dark theme', () async {
     final config = {
-      'accent-color': '#ff0000',
-      'elevated-button-color': '#00ff00',
-      'elevated-button-text-color': '#0000ff',
+      'light': {
+        'accent-color': '#ff0000',
+        'elevated-button-color': '#00ff00',
+        'elevated-button-text-color': '#0000ff',
+      }
     };
     final service =
         ThemeVariantService(config: createMockConfigService(config: config));
@@ -30,6 +32,37 @@ void main() {
         equals(const Color(0xff0000ff)));
   });
 
+  test('getThemeVariant with dark theme', () async {
+    final config = {
+      'light': {
+        'accent-color': '#ff0000',
+        'elevated-button-color': '#00ff00',
+        'elevated-button-text-color': '#0000ff',
+      },
+      'dark': {
+        'accent-color': '#aa0000',
+        'elevated-button-color': '#00aa00',
+        'elevated-button-text-color': '#0000aa',
+      }
+    };
+    final service =
+        ThemeVariantService(config: createMockConfigService(config: config));
+    await service.load();
+    final themeVariant = service.themeVariant;
+    expect(themeVariant, isNotNull);
+    expect(themeVariant!.theme?.primaryColor, equals(const Color(0xffff0000)));
+    expect(themeVariant.theme?.elevatedButtonColor,
+        equals(const Color(0xff00ff00)));
+    expect(themeVariant.theme?.elevatedButtonTextColor,
+        equals(const Color(0xff0000ff)));
+    expect(
+        themeVariant.darkTheme?.primaryColor, equals(const Color(0xffaa0000)));
+    expect(themeVariant.darkTheme?.elevatedButtonColor,
+        equals(const Color(0xff00aa00)));
+    expect(themeVariant.darkTheme?.elevatedButtonTextColor,
+        equals(const Color(0xff0000aa)));
+  });
+
   test('getThemeVariant with missing config', () async {
     final service = ThemeVariantService(config: createMockConfigService());
     await service.load();
@@ -42,8 +75,9 @@ void main() {
 
 MockConfigService createMockConfigService({Map<String, dynamic>? config}) {
   final mock = MockConfigService();
-  when(mock.get('theme'))
-      .thenAnswer((_) async => config ?? <String, dynamic>{});
+  when(mock.get(any, scopeOverride: 'theme')).thenAnswer((i) async =>
+      config?[i.positionalArguments.single] as Map<String, dynamic>? ??
+      <String, dynamic>{});
   return mock;
 }
 
