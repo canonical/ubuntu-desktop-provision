842753619766b9bfeeeb46d175f5fb06e1b689fe
Lukas Klingsbo <lukas.klingsbo@gmail.com>
2024-02-13 13:57:08 +0100

    feat: Use AudioPlayers instead of canberra-gtk-play

--- packages/ubuntu_provision/lib/src/services/sound_service.dart
+++ packages/ubuntu_provision/lib/src/services/sound_service.dart
@@ -1,7 +1,29 @@
-import 'dart:io';
+import 'package:audioplayers/audioplayers.dart';
+import 'package:ubuntu_logger/ubuntu_logger.dart';
+
+final _log = Logger('sounds_service');
+
+enum SoundId {
+  systemReady('system-ready.oga');
+
+  const SoundId(this.filename);
+
+  final String filename;
+}
 
 class SoundService {
-  Future<void> play(String id) async {
-    await Process.run('canberra-gtk-play', ['--id=$id']);
+  Future<void> load() {
+    _log.info('Loading sound assets');
+    // TODO(Lukas): Add support for loading sounds defined in the whitelabel config
+    AudioCache.instance =
+        AudioCache(prefix: 'packages/ubuntu_provision/assets/sounds/');
+    return AudioCache.instance.loadAll([
+      'system-ready.oga',
+    ]);
+  }
+
+  Future<void> play(SoundId id) async {
+    _log.info('Playing sound ${id.filename}');
+    await AudioPlayer().play(AssetSource(id.filename));
   }
 }
