7595811c6f5ebc75e981490f90fd7654f656be93
Lukas Klingsbo <lukas.klingsbo@gmail.com>
2024-02-16 15:23:32 +0100

    fix: Don't crash if canberra-gtk-play doesn't exist

--- packages/ubuntu_provision/lib/src/services/sound_service.dart
+++ packages/ubuntu_provision/lib/src/services/sound_service.dart
@@ -1,7 +1,18 @@
 import 'dart:io';
 
+import 'package:ubuntu_logger/ubuntu_logger.dart';
+
+final _log = Logger('sound_service');
+
+/// A service for playing sounds.
 class SoundService {
   Future<void> play(String id) async {
-    await Process.run('canberra-gtk-play', ['--id=$id']);
+    try {
+      await Process.run('canberra-gtk-play', ['--id=$id']);
+    } on ProcessException catch (e) {
+      _log.error(
+        'Error playing sound with id: $id (most likely missing canberra-gtk-play)',
+      );
+    }
   }
 }
