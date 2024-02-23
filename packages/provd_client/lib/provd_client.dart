d02512770ac1fea109c6f495e74c6fd42e93f0a0
Dennis Loose <dennis.loose@canonical.com>
2024-02-23 11:59:51 +0100

    feat(provd_client): add ProServiceClient

--- packages/provd_client/lib/provd_client.dart
+++ packages/provd_client/lib/provd_client.dart
@@ -1,5 +1,6 @@
 export 'src/provd_keyboard_client.dart';
 export 'src/provd_locale_client.dart';
 export 'src/provd_privacy_client.dart';
+export 'src/provd_pro_client.dart';
 export 'src/provd_timezone_client.dart';
 export 'src/provd_user_client.dart';
--- packages/provd_client/test/test_utils.dart
+++ packages/provd_client/test/test_utils.dart
@@ -15,3 +15,26 @@ class MockResponseFuture<T> extends Mock implements ResponseFuture<T> {
         onError: onError,
       );
 }
+
+class MockResponseStream<T> extends Mock implements ResponseStream<T> {
+  MockResponseStream(this.stream);
+
+  final Stream<T> stream;
+
+  @override
+  Stream<T> asBroadcastStream(
+      {void Function(StreamSubscription<T> subscription)? onListen,
+      void Function(StreamSubscription<T> subscription)? onCancel}) {
+    return stream.asBroadcastStream(onListen: onListen, onCancel: onCancel);
+  }
+
+  @override
+  StreamSubscription<T> listen(void Function(T value)? onData,
+      {Function? onError, void Function()? onDone, bool? cancelOnError}) {
+    return stream.listen(onData,
+        onError: onError, onDone: onDone, cancelOnError: cancelOnError);
+  }
+
+  @override
+  bool get isBroadcast => stream.isBroadcast;
+}
