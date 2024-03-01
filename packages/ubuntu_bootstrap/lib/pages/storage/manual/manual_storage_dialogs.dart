d879996c23dc49f3245982f89b0e1f2a82a04b22
Lukas Klingsbo <lukas.klingsbo@gmail.com>
2024-03-01 15:19:41 +0100

    Remove unused imports

--- packages/ubuntu_bootstrap/lib/pages/storage/manual/manual_storage_dialogs.dart
+++ packages/ubuntu_bootstrap/lib/pages/storage/manual/manual_storage_dialogs.dart
@@ -1,6 +1,4 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:ubuntu_bootstrap/l10n.dart';
 import 'package:ubuntu_bootstrap/pages/storage/manual/manual_storage_model.dart';
