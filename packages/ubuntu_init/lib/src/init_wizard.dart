a2b9c26f06ccbc2bcd2f02ebc4dcaf25117f7f46
Lukas Klingsbo <lukas.klingsbo@gmail.com>
2024-03-21 19:34:44 +0100

    feat: New ErrorPage design

--- packages/ubuntu_init/lib/src/init_wizard.dart
+++ packages/ubuntu_init/lib/src/init_wizard.dart
@@ -2,11 +2,12 @@ import 'dart:async';
 
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:ubuntu_init/src/error_page.dart';
 import 'package:ubuntu_init/src/init_model.dart';
 import 'package:ubuntu_init/src/init_pages.dart';
 import 'package:ubuntu_init/src/init_step.dart';
 import 'package:ubuntu_logger/ubuntu_logger.dart';
-import 'package:ubuntu_provision/ubuntu_provision.dart';
+import 'package:ubuntu_provision/ubuntu_provision.dart' hide ErrorPage;
 import 'package:ubuntu_wizard/ubuntu_wizard.dart';
 import 'package:yaru/yaru.dart';
 
