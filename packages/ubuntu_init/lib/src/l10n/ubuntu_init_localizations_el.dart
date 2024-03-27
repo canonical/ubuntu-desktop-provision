c4569f706e6cbe3f58a7e0d24214b0c62a985acc
Enzo Conty <contact@enzoconty.dev>
2024-03-27 15:07:51 +0100

    generated translations

--- packages/ubuntu_init/lib/src/l10n/ubuntu_init_localizations_el.dart
+++ packages/ubuntu_init/lib/src/l10n/ubuntu_init_localizations_el.dart
@@ -93,4 +93,48 @@ class UbuntuInitLocalizationsEl extends UbuntuInitLocalizations {
 
   @override
   String get ubuntuProHeader => 'Attach this machine';
+
+  @override
+  String get ubuntuProOrAddTokenManually => 'or add a token manually';
+
+  @override
+  String get ubuntuProTokenTextfieldHint => 'Token';
+
+  @override
+  String get ubuntuProSubscriptionDescription => 'Get the most comprehensive subscription for open-source software security';
+
+  @override
+  String ubuntuProSubscriptionFreeForPersonalUse(Object url) {
+    return 'Always free for personal use. 30-days trial for enterprises. <a href=\"https://$url\">Learn more</a>';
+  }
+
+  @override
+  String get ubuntuProTokenAttachButton => 'Attach';
+
+  @override
+  String get ubuntuProTokenAttachError => 'Invalid token';
+
+  @override
+  String get ubuntuProTokenAttachSucess => 'Valid token';
+
+  @override
+  String get ubuntuProTokenAttachErrorVariant => 'Invalid token, retry';
+
+  @override
+  String get ubuntuProOnBoardingSkipForNow => 'Skip for now';
+
+  @override
+  String get ubuntuProOnBoardingSkipForNowDescription => 'You can always enable Ubuntu Pro later via the Software & Updates application.';
+
+  @override
+  String get ubuntuProOnBoardingEnableUbuntuPro => 'Enable Ubuntu Pro';
+
+  @override
+  String get ubuntuProOnBoardingEnableUbuntuProDescription => 'Attach this machine to an Ubuntu Pro subscription for security updates on a wider range of packages, until 2032.';
+
+  @override
+  String get ubuntuProSucessAttachHeader => 'Ubuntu Pro is now enabled in this machine';
+
+  @override
+  String get ubuntuProSucessAttachDescription => 'You can manage your Pro services in the Software & Updates application.';
 }
