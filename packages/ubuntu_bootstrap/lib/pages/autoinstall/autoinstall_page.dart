4d7cb7dcfa45e082c0c4352e58b5f9564be51f23
Lukas Klingsbo <lukas.klingsbo@gmail.com>
2024-04-11 15:55:58 +0200

    fix: Only show validate button if url is valid

--- packages/ubuntu_bootstrap/lib/pages/autoinstall/autoinstall_page.dart
+++ packages/ubuntu_bootstrap/lib/pages/autoinstall/autoinstall_page.dart
@@ -113,7 +113,7 @@ class _ValidateButton extends StatelessWidget {
       style: theme.elevatedButtonTheme.style?.copyWith(
         minimumSize: MaterialStateProperty.all(kPushButtonSize),
       ),
-      onPressed: model.url.isNotEmpty ? model.apply : null,
+      onPressed: _isValidUrl(model.url) ? model.apply : null,
       child: Row(
         mainAxisSize: MainAxisSize.min,
         children: [
@@ -131,5 +131,15 @@ class _ValidateButton extends StatelessWidget {
         ],
       ),
     );
+  }
+
+  bool _isValidUrl(String url) {
+    final parsedUrl = Uri.tryParse(url);
+    if (parsedUrl == null) {
+      return false;
+    }
+    return parsedUrl.hasAbsolutePath &&
+        !parsedUrl.hasEmptyPath &&
+        parsedUrl.pathSegments.isNotEmpty;
   }
 }
