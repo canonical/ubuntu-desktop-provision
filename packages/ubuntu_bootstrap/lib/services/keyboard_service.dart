ce550026cd230089ef643dd933ffade1bab9173a
Dennis Loose <dennis.loose@canonical.com>
2024-04-11 11:48:49 +0200

    feat(bootstrap): set input source via gsettings

--- packages/ubuntu_bootstrap/lib/services/keyboard_service.dart
+++ packages/ubuntu_bootstrap/lib/services/keyboard_service.dart
@@ -1,10 +1,22 @@
+import 'package:dbus/dbus.dart';
+import 'package:gsettings/gsettings.dart';
+import 'package:meta/meta.dart';
 import 'package:subiquity_client/subiquity_client.dart';
+import 'package:ubuntu_bootstrap/ubuntu_bootstrap.dart';
+import 'package:ubuntu_logger/ubuntu_logger.dart';
 import 'package:ubuntu_provision/services.dart';
 
+final _log = Logger('keyboard_service');
+
 class SubiquityKeyboardService implements KeyboardService {
-  const SubiquityKeyboardService(this._subiquity);
+  SubiquityKeyboardService(
+    this._subiquity, {
+    @visibleForTesting GSettings? inputSourceSettings,
+  }) : _inputSourceSettings = inputSourceSettings ??
+            createService<GSettings, String>('org.gnome.desktop.input-sources');
 
   final SubiquityClient _subiquity;
+  final GSettings _inputSourceSettings;
 
   @override
   Future<KeyboardSetup> getKeyboard() => _subiquity.getKeyboard();
@@ -15,7 +27,8 @@ class SubiquityKeyboardService implements KeyboardService {
   }
 
   @override
-  Future<void> setInputSource(KeyboardSetting setting, {String? user}) {
+  Future<void> setInputSource(KeyboardSetting setting, {String? user}) async {
+    await _setGsettingsInputSource(setting);
     return _subiquity.setInputSource(setting, user: user);
   }
 
@@ -26,4 +39,22 @@ class SubiquityKeyboardService implements KeyboardService {
 
   @override
   bool get canDetectLayout => true;
+
+  Future<void> _setGsettingsInputSource(KeyboardSetting setting) async {
+    try {
+      final xkbString = setting.variant.isNotEmpty
+          ? '${setting.layout}+${setting.variant}'
+          : setting.layout;
+      await _inputSourceSettings.set(
+        'sources',
+        DBusArray(
+            DBusSignature.struct([DBusSignature.string, DBusSignature.string]),
+            [
+              DBusStruct([const DBusString('xkb'), DBusString(xkbString)])
+            ]),
+      );
+    } on Exception catch (e) {
+      _log.error('Failed to set input source via gsettings', e);
+    }
+  }
 }
