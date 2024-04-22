1c9575b1ac6e969eeda4a56e6200c2b1abf6323b
Enzo Conty <contact@enzoconty.dev>
2024-04-22 17:09:37 +0200

    feat: consistancy for local format.

--- packages/ubuntu_provision/lib/src/eula/eula_page.dart
+++ packages/ubuntu_provision/lib/src/eula/eula_page.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:intl/intl.dart';
 import 'package:intl/intl_standalone.dart';
-import 'package:path/path.dart';
+import 'package:path/path.dart' as p;
 import 'package:pdfrx/pdfrx.dart';
 import 'package:ubuntu_logger/ubuntu_logger.dart';
 import 'package:ubuntu_provision/interfaces.dart';
@@ -13,18 +13,11 @@ import 'package:ubuntu_wizard/ubuntu_wizard.dart';
 import 'package:yaru/yaru.dart';
 
 final eulaPageProvider = FutureProvider<File>((ref) async {
-  final selectedLocale = Locale(Intl.defaultLocale ?? await findSystemLocale());
-  final fileList = Directory('/usr/share/desktop-provision/eula').listSync();
-  final exp = RegExp(r'EULA_([a-zA-Z0-9\-]+)\.');
-  final supportedLocales = fileList
-      .map((entry) => basename(entry.path))
-      .where(exp.hasMatch)
-      .map((entry) => Locale(exp.firstMatch(entry)!.group(1)!));
-
-  final locale = basicLocaleListResolution([selectedLocale], supportedLocales);
-  final eulaFile = File(
-      '/usr/share/desktop-provision/eula/EULA_${locale.toLanguageTag()}.pdf');
-
+  final locale = Intl.defaultLocale ?? await findSystemLocale();
+  const directory = '/usr/share/desktop-provision/eula';
+  final localizedEula = File(p.join(directory, 'EULA_$locale.pdf'));
+  late final fallbackEula = File(p.join(directory, 'EULA.pdf'));
+  final eulaFile = localizedEula.existsSync() ? localizedEula : fallbackEula;
   Logger('eula').debug('EULA file: ${eulaFile.path}');
 
   return eulaFile;
