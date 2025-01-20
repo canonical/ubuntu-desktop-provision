ca3d79c9ae138f1923438a111a8f0be3805b7c32
Matthew Hagemann <matt.hagemann@canonical.com>
2025-01-20 16:33:24 +0200

    chore: rebasing off main

--- apps/ubuntu_bootstrap/lib/pages/landscape/landscape_model.dart
+++ apps/ubuntu_bootstrap/lib/pages/landscape/landscape_model.dart
@@ -1,64 +1,94 @@
 import 'dart:async';
 
 import 'package:flutter/material.dart';
+import 'package:freezed_annotation/freezed_annotation.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:riverpod_annotation/riverpod_annotation.dart';
 import 'package:ubuntu_bootstrap/services/landscape_service.dart';
 import 'package:ubuntu_service/ubuntu_service.dart';
 
-final landscapeModelProvider = ChangeNotifierProvider(
-  (ref) => LandscapeModel(
-    landscapeService: getService<LandscapeService>(),
-  ),
-);
+part 'landscape_model.freezed.dart';
+part 'landscape_model.g.dart';
 
-class LandscapeModel extends ChangeNotifier {
-  LandscapeModel({
-    required LandscapeService landscapeService,
-  });
+@freezed
+class LandscapeData with _$LandscapeData {
+  factory LandscapeData({
+    required String userCode,
+    required bool isAttachedThroughMagicAttach,
+    required bool isAttached,
+    required bool isAttachedThroughManualAttach,
+    required bool hasNoErrorWhenAttachingManually,
+    required bool skipPro,
+  }) = _LandscapeData;
 
-  String get userCode => _userCode;
-  // FIXME: return this default to ''
-  final String _userCode = 'ABCD EFGH';
-
-  bool get isAttachedThroughMagicAttach => _isAttachedThroughMagicAttach;
-  final bool _isAttachedThroughMagicAttach = false;
-
-  bool get isAttached => _isAttached;
-  // FIXME: return this default to false
-  bool _isAttached = true;
-
-  bool get isAttachedThroughManualAttach => _isAttachedThroughManualAttach;
-  final bool _isAttachedThroughManualAttach = false;
-
-  bool get hasNoErrorWhenAttachingManually => _hasNoErrorWhenAttachingManually;
-  final bool _hasNoErrorWhenAttachingManually = true;
-
-  bool get skipPro => _skipPro;
-  bool _skipPro = false;
-
-  set skipPro(bool value) {
-    _skipPro = value;
-    notifyListeners();
-  }
-
-  set isAttached(bool value) {
-    _isAttached = value;
-    notifyListeners();
-  }
-
-  final _token = ValueNotifier<String?>(null);
-
-  /// The token input from the user
-  String get token => _token.value ?? '';
-
-  void setToken(String value) {
-    _token.value = value;
-    notifyListeners();
-  }
-
-  // StreamSubscription<ProResponse>? _subscription;
-
-  Future<void> magicAttach() async {}
-
-  Future<void> attachManuallyToken() async {}
+  LandscapeData._();
 }
+
+@riverpod
+class LandscapeDataModel extends _$LandscapeDataModel {
+  @override
+  LandscapeData build() {
+    return LandscapeData(
+      userCode: 'ABCD EFGH',
+      isAttachedThroughMagicAttach: false,
+      isAttached: true,
+      isAttachedThroughManualAttach: false,
+      hasNoErrorWhenAttachingManually: true,
+      skipPro: false,
+    );
+  }
+
+  void setSkipPro(bool value) {
+    if (state.skipPro == value) return;
+    state = state.copyWith(skipPro: value);
+  }
+  
+  void setIsAttached(bool value) {
+    if (state.isAttached == value) return;
+    state = state.copyWith(isAttached: value);
+  }
+}
+
+//   set isAttached(bool value) {
+//     _isAttached = value;
+//     notifyListeners();
+//   }
+
+//   final _token = ValueNotifier<String?>(null);
+
+//   /// The token input from the user
+//   String get token => _token.value ?? '';
+
+//   // example of where we will use copyWith state...
+//   void setToken(String value) {
+//     _token.value = value;
+//     notifyListeners();
+//   }
+
+//   // StreamSubscription<ProResponse>? _subscription;
+
+//   Future<void> magicAttach() async {}
+
+//   Future<void> attachManuallyToken() async {}
+// }
+
+//   // FIXME: will end up in the @freezed model, returend in the build method
+//   String get userCode => _userCode;
+//   // FIXME: return this default to ''
+//   final String _userCode = 'ABCD EFGH';
+
+//   bool get isAttachedThroughMagicAttach => _isAttachedThroughMagicAttach;
+//   final bool _isAttachedThroughMagicAttach = false;
+
+//   bool get isAttached => _isAttached;
+//   // FIXME: return this default to false
+//   bool _isAttached = true;
+
+//   bool get isAttachedThroughManualAttach => _isAttachedThroughManualAttach;
+//   final bool _isAttachedThroughManualAttach = false;
+
+//   bool get hasNoErrorWhenAttachingManually => _hasNoErrorWhenAttachingManually;
+//   final bool _hasNoErrorWhenAttachingManually = true;
+
+//   bool get skipPro => _skipPro;
+//   bool _skipPro = false;
---
+++ apps/ubuntu_bootstrap/lib/pages/landscape/landscape_model.freezed.dart
@@ -0,0 +1,278 @@
+// coverage:ignore-file
+// GENERATED CODE - DO NOT MODIFY BY HAND
+// ignore_for_file: type=lint
+// ignore_for_file: unused_element, deprecated_member_use, deprecated_member_use_from_same_package, use_function_type_syntax_for_parameters, unnecessary_const, avoid_init_to_null, invalid_override_different_default_values_named, prefer_expression_function_bodies, annotate_overrides, invalid_annotation_target, unnecessary_question_mark
+
+part of 'landscape_model.dart';
+
+// **************************************************************************
+// FreezedGenerator
+// **************************************************************************
+
+T _$identity<T>(T value) => value;
+
+final _privateConstructorUsedError = UnsupportedError(
+    'It seems like you constructed your class using `MyClass._()`. This constructor is only meant to be used by freezed and you are not supposed to need it nor use it.\nPlease check the documentation here for more information: https://github.com/rrousselGit/freezed#adding-getters-and-methods-to-our-models');
+
+/// @nodoc
+mixin _$LandscapeData {
+  String get userCode => throw _privateConstructorUsedError;
+  bool get isAttachedThroughMagicAttach => throw _privateConstructorUsedError;
+  bool get isAttached => throw _privateConstructorUsedError;
+  bool get isAttachedThroughManualAttach => throw _privateConstructorUsedError;
+  bool get hasNoErrorWhenAttachingManually =>
+      throw _privateConstructorUsedError;
+  bool get skipPro => throw _privateConstructorUsedError;
+  String? get token => throw _privateConstructorUsedError;
+
+  @JsonKey(ignore: true)
+  $LandscapeDataCopyWith<LandscapeData> get copyWith =>
+      throw _privateConstructorUsedError;
+}
+
+/// @nodoc
+abstract class $LandscapeDataCopyWith<$Res> {
+  factory $LandscapeDataCopyWith(
+          LandscapeData value, $Res Function(LandscapeData) then) =
+      _$LandscapeDataCopyWithImpl<$Res, LandscapeData>;
+  @useResult
+  $Res call(
+      {String userCode,
+      bool isAttachedThroughMagicAttach,
+      bool isAttached,
+      bool isAttachedThroughManualAttach,
+      bool hasNoErrorWhenAttachingManually,
+      bool skipPro,
+      String? token});
+}
+
+/// @nodoc
+class _$LandscapeDataCopyWithImpl<$Res, $Val extends LandscapeData>
+    implements $LandscapeDataCopyWith<$Res> {
+  _$LandscapeDataCopyWithImpl(this._value, this._then);
+
+  // ignore: unused_field
+  final $Val _value;
+  // ignore: unused_field
+  final $Res Function($Val) _then;
+
+  @pragma('vm:prefer-inline')
+  @override
+  $Res call({
+    Object? userCode = null,
+    Object? isAttachedThroughMagicAttach = null,
+    Object? isAttached = null,
+    Object? isAttachedThroughManualAttach = null,
+    Object? hasNoErrorWhenAttachingManually = null,
+    Object? skipPro = null,
+    Object? token = freezed,
+  }) {
+    return _then(_value.copyWith(
+      userCode: null == userCode
+          ? _value.userCode
+          : userCode // ignore: cast_nullable_to_non_nullable
+              as String,
+      isAttachedThroughMagicAttach: null == isAttachedThroughMagicAttach
+          ? _value.isAttachedThroughMagicAttach
+          : isAttachedThroughMagicAttach // ignore: cast_nullable_to_non_nullable
+              as bool,
+      isAttached: null == isAttached
+          ? _value.isAttached
+          : isAttached // ignore: cast_nullable_to_non_nullable
+              as bool,
+      isAttachedThroughManualAttach: null == isAttachedThroughManualAttach
+          ? _value.isAttachedThroughManualAttach
+          : isAttachedThroughManualAttach // ignore: cast_nullable_to_non_nullable
+              as bool,
+      hasNoErrorWhenAttachingManually: null == hasNoErrorWhenAttachingManually
+          ? _value.hasNoErrorWhenAttachingManually
+          : hasNoErrorWhenAttachingManually // ignore: cast_nullable_to_non_nullable
+              as bool,
+      skipPro: null == skipPro
+          ? _value.skipPro
+          : skipPro // ignore: cast_nullable_to_non_nullable
+              as bool,
+      token: freezed == token
+          ? _value.token
+          : token // ignore: cast_nullable_to_non_nullable
+              as String?,
+    ) as $Val);
+  }
+}
+
+/// @nodoc
+abstract class _$$LandscapeDataImplCopyWith<$Res>
+    implements $LandscapeDataCopyWith<$Res> {
+  factory _$$LandscapeDataImplCopyWith(
+          _$LandscapeDataImpl value, $Res Function(_$LandscapeDataImpl) then) =
+      __$$LandscapeDataImplCopyWithImpl<$Res>;
+  @override
+  @useResult
+  $Res call(
+      {String userCode,
+      bool isAttachedThroughMagicAttach,
+      bool isAttached,
+      bool isAttachedThroughManualAttach,
+      bool hasNoErrorWhenAttachingManually,
+      bool skipPro,
+      String? token});
+}
+
+/// @nodoc
+class __$$LandscapeDataImplCopyWithImpl<$Res>
+    extends _$LandscapeDataCopyWithImpl<$Res, _$LandscapeDataImpl>
+    implements _$$LandscapeDataImplCopyWith<$Res> {
+  __$$LandscapeDataImplCopyWithImpl(
+      _$LandscapeDataImpl _value, $Res Function(_$LandscapeDataImpl) _then)
+      : super(_value, _then);
+
+  @pragma('vm:prefer-inline')
+  @override
+  $Res call({
+    Object? userCode = null,
+    Object? isAttachedThroughMagicAttach = null,
+    Object? isAttached = null,
+    Object? isAttachedThroughManualAttach = null,
+    Object? hasNoErrorWhenAttachingManually = null,
+    Object? skipPro = null,
+    Object? token = freezed,
+  }) {
+    return _then(_$LandscapeDataImpl(
+      userCode: null == userCode
+          ? _value.userCode
+          : userCode // ignore: cast_nullable_to_non_nullable
+              as String,
+      isAttachedThroughMagicAttach: null == isAttachedThroughMagicAttach
+          ? _value.isAttachedThroughMagicAttach
+          : isAttachedThroughMagicAttach // ignore: cast_nullable_to_non_nullable
+              as bool,
+      isAttached: null == isAttached
+          ? _value.isAttached
+          : isAttached // ignore: cast_nullable_to_non_nullable
+              as bool,
+      isAttachedThroughManualAttach: null == isAttachedThroughManualAttach
+          ? _value.isAttachedThroughManualAttach
+          : isAttachedThroughManualAttach // ignore: cast_nullable_to_non_nullable
+              as bool,
+      hasNoErrorWhenAttachingManually: null == hasNoErrorWhenAttachingManually
+          ? _value.hasNoErrorWhenAttachingManually
+          : hasNoErrorWhenAttachingManually // ignore: cast_nullable_to_non_nullable
+              as bool,
+      skipPro: null == skipPro
+          ? _value.skipPro
+          : skipPro // ignore: cast_nullable_to_non_nullable
+              as bool,
+      token: freezed == token
+          ? _value.token
+          : token // ignore: cast_nullable_to_non_nullable
+              as String?,
+    ));
+  }
+}
+
+/// @nodoc
+
+class _$LandscapeDataImpl extends _LandscapeData {
+  _$LandscapeDataImpl(
+      {required this.userCode,
+      required this.isAttachedThroughMagicAttach,
+      required this.isAttached,
+      required this.isAttachedThroughManualAttach,
+      required this.hasNoErrorWhenAttachingManually,
+      required this.skipPro,
+      this.token})
+      : super._();
+
+  @override
+  final String userCode;
+  @override
+  final bool isAttachedThroughMagicAttach;
+  @override
+  final bool isAttached;
+  @override
+  final bool isAttachedThroughManualAttach;
+  @override
+  final bool hasNoErrorWhenAttachingManually;
+  @override
+  final bool skipPro;
+  @override
+  final String? token;
+
+  @override
+  String toString() {
+    return 'LandscapeData(userCode: $userCode, isAttachedThroughMagicAttach: $isAttachedThroughMagicAttach, isAttached: $isAttached, isAttachedThroughManualAttach: $isAttachedThroughManualAttach, hasNoErrorWhenAttachingManually: $hasNoErrorWhenAttachingManually, skipPro: $skipPro, token: $token)';
+  }
+
+  @override
+  bool operator ==(Object other) {
+    return identical(this, other) ||
+        (other.runtimeType == runtimeType &&
+            other is _$LandscapeDataImpl &&
+            (identical(other.userCode, userCode) ||
+                other.userCode == userCode) &&
+            (identical(other.isAttachedThroughMagicAttach,
+                    isAttachedThroughMagicAttach) ||
+                other.isAttachedThroughMagicAttach ==
+                    isAttachedThroughMagicAttach) &&
+            (identical(other.isAttached, isAttached) ||
+                other.isAttached == isAttached) &&
+            (identical(other.isAttachedThroughManualAttach,
+                    isAttachedThroughManualAttach) ||
+                other.isAttachedThroughManualAttach ==
+                    isAttachedThroughManualAttach) &&
+            (identical(other.hasNoErrorWhenAttachingManually,
+                    hasNoErrorWhenAttachingManually) ||
+                other.hasNoErrorWhenAttachingManually ==
+                    hasNoErrorWhenAttachingManually) &&
+            (identical(other.skipPro, skipPro) || other.skipPro == skipPro) &&
+            (identical(other.token, token) || other.token == token));
+  }
+
+  @override
+  int get hashCode => Object.hash(
+      runtimeType,
+      userCode,
+      isAttachedThroughMagicAttach,
+      isAttached,
+      isAttachedThroughManualAttach,
+      hasNoErrorWhenAttachingManually,
+      skipPro,
+      token);
+
+  @JsonKey(ignore: true)
+  @override
+  @pragma('vm:prefer-inline')
+  _$$LandscapeDataImplCopyWith<_$LandscapeDataImpl> get copyWith =>
+      __$$LandscapeDataImplCopyWithImpl<_$LandscapeDataImpl>(this, _$identity);
+}
+
+abstract class _LandscapeData extends LandscapeData {
+  factory _LandscapeData(
+      {required final String userCode,
+      required final bool isAttachedThroughMagicAttach,
+      required final bool isAttached,
+      required final bool isAttachedThroughManualAttach,
+      required final bool hasNoErrorWhenAttachingManually,
+      required final bool skipPro,
+      final String? token}) = _$LandscapeDataImpl;
+  _LandscapeData._() : super._();
+
+  @override
+  String get userCode;
+  @override
+  bool get isAttachedThroughMagicAttach;
+  @override
+  bool get isAttached;
+  @override
+  bool get isAttachedThroughManualAttach;
+  @override
+  bool get hasNoErrorWhenAttachingManually;
+  @override
+  bool get skipPro;
+  @override
+  String? get token;
+  @override
+  @JsonKey(ignore: true)
+  _$$LandscapeDataImplCopyWith<_$LandscapeDataImpl> get copyWith =>
+      throw _privateConstructorUsedError;
+}
