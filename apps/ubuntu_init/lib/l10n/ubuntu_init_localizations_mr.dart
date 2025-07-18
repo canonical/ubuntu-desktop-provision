10cd4048c610bf0093dd9f117bece12d5852c65a
ashuntu <ashton.nelson@canonical.com>
2025-07-18 14:40:10 -0500

    ubuntu_init: Add window control semantic labels

--- apps/ubuntu_init/lib/l10n/ubuntu_init_localizations_mr.dart
+++ apps/ubuntu_init/lib/l10n/ubuntu_init_localizations_mr.dart
@@ -155,4 +155,13 @@ class UbuntuInitLocalizationsMr extends UbuntuInitLocalizations {
 
   @override
   String get successIconSemanticLabel => 'Success';
+
+  @override
+  String get closeIconSemanticLabel => 'Close';
+
+  @override
+  String get maximizeIconSemanticLabel => 'Maximize';
+
+  @override
+  String get minimizeIconSemanticLabel => 'Minimize';
 }
