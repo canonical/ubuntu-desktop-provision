abc6ef2d3c52c0d316a074f5c9c17ec57ad10ca2
Dennis Loose <dennis.loose@canonical.com>
2026-02-25 13:16:44 +0100

    feat: show border when scroll view extends beyond visible area

--- packages/ubuntu_provision/lib/src/widgets/horizontal_page.dart
+++ packages/ubuntu_provision/lib/src/widgets/horizontal_page.dart
@@ -7,7 +7,7 @@ import 'package:ubuntu_provision/providers.dart';
 import 'package:ubuntu_wizard/ubuntu_wizard.dart';
 import 'package:yaru/yaru.dart';
 
-class HorizontalPage extends ConsumerWidget {
+class HorizontalPage extends ConsumerStatefulWidget {
   const HorizontalPage({
     required this.windowTitle,
     required this.title,
@@ -76,13 +76,46 @@ class HorizontalPage extends ConsumerWidget {
   final FocusNode? nextFocusNode;
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
+  ConsumerState<HorizontalPage> createState() => _HorizontalPageState();
+}
+
+class _HorizontalPageState extends ConsumerState<HorizontalPage> {
+  final _controller = ScrollController();
+  bool _showDivider = false;
+
+  @override
+  void initState() {
+    super.initState();
+    WidgetsBinding.instance.addPostFrameCallback((_) => _checkScrollState());
+    _controller.addListener(_checkScrollState);
+  }
+
+  @override
+  void dispose() {
+    _controller.removeListener(_checkScrollState);
+    _controller.dispose();
+    super.dispose();
+  }
+
+  void _checkScrollState() {
+    if (!_controller.hasClients) return;
+    final showDivider = _controller.position.extentAfter > 0;
+    if (showDivider != _showDivider) {
+      setState(() {
+        _showDivider = showDivider;
+      });
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
     final name = ModalRoute.of(context)!.settings.name!.replaceFirst('/', '');
     final image = ref.watch(pageImagesProvider).get(name);
     final windowSize = MediaQuery.of(context).size;
     final isSmallWindow = windowSize.width < 700 || windowSize.height < 500;
-    final adjustedPadding =
-        isSmallWindow ? padding.copyWith(right: 0, left: 0) : padding;
+    final adjustedPadding = isSmallWindow
+        ? widget.padding.copyWith(right: 0, left: 0)
+        : widget.padding;
     final scrollBarPadding =
         (ScrollbarTheme.of(context).thickness?.resolve({}) ?? 6) * 4;
     const hoverPadding = EdgeInsets.only(left: 4, bottom: 4);
@@ -90,28 +123,35 @@ class HorizontalPage extends ConsumerWidget {
         EdgeInsets.only(top: 3 * kWizardSpacing, bottom: 3 * kWizardSpacing);
     final lang = UbuntuProvisionLocalizations.of(context);
 
+    // Re-check scroll state after rendering the frame, in case the scrollbar
+    // disappears or re-appears entirely.
+    WidgetsBinding.instance.addPostFrameCallback((_) => _checkScrollState());
+
     return WizardPage(
       title: YaruWindowTitleBar(
-        title: Text(windowTitle),
+        title: Text(widget.windowTitle),
         closeSemanticLabel: lang.closeIconSemanticLabel,
         maximizeSemanticLabel: lang.maximizeIconSemanticLabel,
         minimizeSemanticLabel: lang.minimizeIconSemanticLabel,
       ),
       contentPadding: EdgeInsets.zero,
       headerPadding: EdgeInsets.zero,
-      footer: managedScrolling ? Divider() : null,
+      footer: AnimatedSwitcher(
+        duration: Duration(milliseconds: 100),
+        child: _showDivider ? Divider() : null,
+      ),
       content: Row(
         children: [
-          if (image != null || imageTitleWidget != null)
+          if (image != null || widget.imageTitleWidget != null)
             Expanded(
               flex: 6,
               child: Column(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
                   if (image != null) Flexible(child: image),
-                  if (imageTitleWidget != null) ...[
+                  if (widget.imageTitleWidget != null) ...[
                     const SizedBox(height: kWizardSpacing),
-                    imageTitleWidget!,
+                    widget.imageTitleWidget!,
                   ],
                 ],
               ),
@@ -122,38 +162,43 @@ class HorizontalPage extends ConsumerWidget {
             child: Column(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                if (!managedScrolling) ...[
+                if (!widget.managedScrolling) ...[
                   const Spacer(),
                   Padding(
                     padding: adjustedPadding,
                     child: _Headline(
-                      title: title,
-                      trailingTitleWidget: trailingTitleWidget,
+                      title: widget.title,
+                      trailingTitleWidget: widget.trailingTitleWidget,
                     ),
                   ),
                 ],
                 Expanded(
-                  flex: managedScrolling ? 1 : _contentFlex,
-                  child: managedScrolling
+                  flex: widget.managedScrolling ? 1 : widget._contentFlex,
+                  child: widget.managedScrolling
                       ? Center(
-                          child: Scrollbar(
-                            thumbVisibility: true,
-                            child: SingleChildScrollView(
-                              primary: true,
-                              padding: hoverPadding +
-                                  EdgeInsets.only(right: scrollBarPadding) +
-                                  scrollViewPadding,
-                              child: Padding(
-                                padding: adjustedPadding,
-                                child: Column(
-                                  crossAxisAlignment: CrossAxisAlignment.start,
-                                  children: [
-                                    _Headline(
-                                      title: title,
-                                      trailingTitleWidget: trailingTitleWidget,
-                                    ),
-                                    ...children,
-                                  ],
+                          child: PrimaryScrollController(
+                            controller: _controller,
+                            child: Scrollbar(
+                              thumbVisibility: true,
+                              child: SingleChildScrollView(
+                                primary: true,
+                                padding: hoverPadding +
+                                    EdgeInsets.only(right: scrollBarPadding) +
+                                    scrollViewPadding,
+                                child: Padding(
+                                  padding: adjustedPadding,
+                                  child: Column(
+                                    crossAxisAlignment:
+                                        CrossAxisAlignment.start,
+                                    children: [
+                                      _Headline(
+                                        title: widget.title,
+                                        trailingTitleWidget:
+                                            widget.trailingTitleWidget,
+                                      ),
+                                      ...widget.children,
+                                    ],
+                                  ),
                                 ),
                               ),
                             ),
@@ -161,24 +206,24 @@ class HorizontalPage extends ConsumerWidget {
                         )
                       : Padding(
                           padding: adjustedPadding,
-                          child: Column(children: children),
+                          child: Column(children: widget.children),
                         ),
                 ),
-                if (!managedScrolling) const Spacer(),
+                if (!widget.managedScrolling) const Spacer(),
               ],
             ),
           ),
         ],
       ),
-      snackBar: snackBar,
-      bottomBar: bottomBar ??
+      snackBar: widget.snackBar,
+      bottomBar: widget.bottomBar ??
           WizardBar(
             leading: const BackWizardButton(),
             trailing: [
               NextWizardButton(
-                focusNode: nextFocusNode,
-                onNext: onNext,
-                enabled: isNextEnabled,
+                focusNode: widget.nextFocusNode,
+                onNext: widget.onNext,
+                enabled: widget.isNextEnabled,
               ),
             ],
           ),
